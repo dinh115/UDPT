61d229139345c26c94d68c00c80a8d19144a946e
Doen1x <thituyetnguyen.dmxtk@gmail.com>
2025-06-20 16:59:43 +0700

    simple patient services api

--- source/services/gateway/index.js
+++ source/services/gateway/index.js
@@ -9,33 +9,33 @@ app.use(express.json());
 
 // Proxy configuration for the Patient service
 const PatientServiceUrl = 'http://patient-service:3001';
-app.use('/products', createProxyMiddleware({
-  target: productServiceUrl,
+app.use('/patient', createProxyMiddleware({
+  target: PatientServiceUrl,
   changeOrigin: true,
   pathRewrite: {
-    '^/products': '', // Remove /products prefix when forwarding to the product service
+    '^/patient': '', 
   },
 }));
 
-// Proxy configuration for the employee service
-const EmployeeServiceUrl = 'http://employee-service:3002';
-app.use('/users', createProxyMiddleware({
-  target: userServiceUrl,
-  changeOrigin: true,
-  pathRewrite: {
-    '^/users': '', // Remove /users prefix when forwarding to the user service
-  },
-}));
+// // Proxy configuration for the employee service
+// const EmployeeServiceUrl = 'http://employee-service:3002';
+// app.use('/employee', createProxyMiddleware({
+//   target: userServiceUrl,
+//   changeOrigin: true,
+//   pathRewrite: {
+//     '^/employee': '', 
+//   },
+// }));
 
-// Proxy configuration for the doctor service
-const DoctorServiceUrl = 'http://doctor-service:3003';
-app.use('/doctors', createProxyMiddleware({
-  target: doctorServiceUrl,
-  changeOrigin: true,
-  pathRewrite: {
-    '^/doctors': '', // Remove /doctors prefix when forwarding to the doctor service
-  },
-}));
+// // Proxy configuration for the doctor service
+// const DoctorServiceUrl = 'http://doctor-service:3003';
+// app.use('/doctors', createProxyMiddleware({
+//   target: doctorServiceUrl,
+//   changeOrigin: true,
+//   pathRewrite: {
+//     '^/doctors': '',
+//   },
+// }));
 
 app.listen(PORT, () => {
   console.log(`api-gateway running at http://localhost:${PORT}`);
